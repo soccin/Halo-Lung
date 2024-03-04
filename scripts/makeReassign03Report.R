5a64e351c43cda60a6b28d3583f2c12655bdb5aa
Nicholas D. Socci <soccin@mskcc.org>
2024-03-04 14:00:19 -0500

    Fix report for samples with no reassign

--- scripts/makeReassign03Report.R
+++ scripts/makeReassign03Report.R
@@ -1,10 +1,11 @@
 require(tidyverse)
 require(openxlsx)
 
-dA=fs::dir_ls("out/reassign/03",regex="stats.*Reassign03_A.*.csv") %>% map(read_csv) %>% bind_rows
-dB=fs::dir_ls("out/reassign/03",regex="stats.*Reassign03_B.*.csv") %>% map(read_csv) %>% bind_rows
+dA=fs::dir_ls("out/reassign/03",regex="stats.*Reassign03_A.*.csv") %>% map(read_csv,col_types = cols(.default = "c")) %>% bind_rows %>% type_convert
+dB=fs::dir_ls("out/reassign/03",regex="stats.*Reassign03_B.*.csv") %>% map(read_csv,col_types = cols(.default = "c")) %>% bind_rows %>% type_convert
 
 rpt=dB %>%
-    left_join(dA %>% select(Sample,FOV,Total) %>% distinct)
+    full_join(dA %>% select(Sample,FOV,Total) %>% distinct) %>% 
+    arrange(gsub("L_","",Sample) %>% gsub("[^0-9]","",.) %>% as.numeric)
 
 write.xlsx(list(FOV=rpt),"rpt_Reassign03_A.xlsx")
