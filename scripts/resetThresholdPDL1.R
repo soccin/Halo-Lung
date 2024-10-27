a427435a75cf22e06f5f45b24146cb5c63cdf698
Nicholas D. Socci <soccin@mskcc.org>
2024-10-26 20:25:24 -0400

    Added reset PDL1 stuff

---
+++ scripts/resetThresholdPDL1.R
@@ -0,0 +1,76 @@
+#######################################################################
+argv=commandArgs(trailing=T)
+
+if(length(argv)<2) {
+  cat("\n\n")
+  cat("   usage: resetThresholdPDL1.R reassign.RDA newPDL1Threshold\n")
+  cat("\n\n")
+  quit()
+}
+
+require(tidyverse)
+cellDiveId=argv[1]
+rdaFile=fs::dir_ls("rda/v6_Reassign",regex=cc(cellDiveId,"__Halo"))
+
+newPDL1Threshold=argv[2]
+
+if(newPDL1Threshold=="NA") {
+  cat("\n\nNew threshold is NA just copying the original rda file\n\n")
+  fs::dir_create("rda/v6_Reset")
+  fs::file_copy(rdaFile,fs::path("rda/v6_Reset",basename(rdaFile)),overwrite=T)
+  quit()
+}
+
+newPDL1Threshold=as.numeric(newPDL1Threshold)
+
+rdaFile=fs::dir_ls("rda/v6_Reassign",regex=cc(cellDiveId,"__Halo"))
+oo=readRDS(rdaFile)
+
+thetaOrig=oo$thetas %>%
+  filter(Marker=="PDL1") %>%
+  pull(dye_cyto_positive_threshold_weak)
+
+
+metaData=list(theta_PLD1_orig=thetaOrig)
+oo$metadata$resetThetaPDL1=metaData
+
+oo$thetas=oo$thetas %>%
+  rows_update(
+    tibble(
+      Marker="PDL1",
+      dye_cyto_positive_threshold_weak=newPDL1Threshold
+    ),by="Marker"
+  )
+
+if(newPDL1Threshold<thetaOrig) {
+  resetTbl=oo$marker.data %>%
+    filter(Marker=="PDL1") %>%
+    select(UUID,Positive_Classification,TIntensity) %>%
+    mutate(Positive_Classification=ifelse(TIntensity>newPDL1Threshold,1,Positive_Classification)) %>%
+    select(-TIntensity)
+} else {
+  resetTbl=oo$marker.data %>%
+    filter(Marker=="PDL1") %>%
+    select(UUID,Positive_Classification,TIntensity) %>%
+    mutate(Positive_Classification=ifelse(TIntensity<newPDL1Threshold,0,Positive_Classification)) %>%
+    select(-TIntensity)
+}
+
+oo$marker.data=oo$marker.data %>%
+  mutate(Positive_Classification.orig2=Positive_Classification) %>%
+  rows_update(resetTbl,by="UUID")
+
+stats=oo$marker.data %>%
+  filter(Marker=="PDL1") %>%
+  select(UUID,Marker,Positive_Classification.orig2,Positive_Classification) %>%
+  count(Positive_Classification.orig2,Positive_Classification) %>%
+  rename(ORIG=Positive_Classification.orig2,RESET=Positive_Classification) %>%
+  mutate(CellDive_ID=oo$sample.data$CellDive_ID,Patient_ID=oo$sample.data$Patient_ID)
+
+fs::dir_create("out/resetPDL1")
+write_csv(stats,fs::path("out/resetPDL1",cc(cellDiveId,"_resetPDL1_stats.csv")))
+
+fs::dir_create("rda/v6_Reset")
+saveRDS(oo,fs::path("rda/v6_Reset",basename(rdaFile)),compress=T)
+
+
